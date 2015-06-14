248e0d5e6e9a77759ca287f8801b38389d844b7a
Frank McSherry <fmcsherry@me.com>
2015-06-14 15:47:11 +0100

    updated readme, bfs example

--- README.md
+++ README.md
@@ -22,8 +22,8 @@ Each known minimal distance (to some node) can be joined with the set of edges e
 The program to do this in differential dataflow follows exactly this pattern. Although there is a bit of syntactic guff, and there is no reason you should expect to understand the arguments of the various methods at this point, the above algorithm looks like:
 
 ```rust
-let edges = dataflow.new_input::<((u32, u32), i32)>();
-let roots = dataflow.new_input::<(u32, i32)>();
+let (e_in, edges) = dataflow.new_input::<((u32, u32), i32)>();
+let (r_in, roots) = dataflow.new_input::<(u32, i32)>();
 
 // initialize roots at distance 0
 let start = roots.map(|(x, w)| ((x, 0), w));
@@ -46,6 +46,132 @@ let limit = start.iterate(u32::max_value(), |x| x.0, |x| x.0, |dists| {
 });
 ```
 
-Having defined the computation, we can now modify the `edges` and `roots` input collections. We might start by adding all of our edges, and perhaps then exploring the set of roots. Or we might change the edges with the roots held fixed to see how the distances change for each edge.
+Having defined the computation, we can now modify the `edges` and `roots` input collections, using the `e_in` and `r_in` handles, respectively. We might start by adding all of our edges, and perhaps then exploring the set of roots. Or we might change the edges with the roots held fixed to see how the distances change for each edge.
 
 In any case, the outputs will precisely track the computation applied to the inputs. It's just that easy!
+
+## An example execution: breadth first search
+
+Let's take the [BFS example]() from the differential dataflow repository. It constructs a random graph on 200M edges, performs the reachability computation defined above, starting from root nodes 0,1, and 2, and reports the number of nodes at each distance.
+
+The random number generator is seeded, so you should see the same output I see (up to timings):
+
+```
+Echidnatron% cargo run --release --example bfs
+     Running `target/release/examples/bfs`
+performing BFS on 100000000 nodes, 200000000 edges:
+observed at (Root, 0):
+elapsed: 264.0985786569945s
+	(0, 3)
+	(1, 8)
+	(2, 21)
+	(3, 52)
+	(4, 99)
+	(5, 199)
+	(6, 357)
+	(7, 711)
+	(8, 1349)
+	(9, 2721)
+	(10, 5465)
+	(11, 10902)
+	(12, 21798)
+	(13, 43391)
+	(14, 86149)
+	(15, 172251)
+	(16, 342484)
+	(17, 676404)
+	(18, 1325526)
+	(19, 2544386)
+	(20, 4699159)
+	(21, 8077892)
+	(22, 12229347)
+	(23, 15145774)
+	(24, 14275975)
+	(25, 10020299)
+	(26, 5505139)
+	(27, 2584731)
+	(28, 1118823)
+	(29, 467071)
+	(30, 192398)
+	(31, 78933)
+	(32, 32175)
+	(33, 13103)
+	(34, 5162)
+	(35, 2051)
+	(36, 819)
+	(37, 336)
+	(38, 138)
+	(39, 56)
+	(40, 34)
+	(41, 14)
+	(42, 6)
+	(43, 3)
+	(44, 2)
+```
+
+The computation takes 264.1 seconds to get done, which is not a great (or even good) time for breadth first search on 200 million edges. The computation is single threaded, and the number would go down when more threads are brought to bear.
+
+That isn't the cool part, though.
+
+### Concurrent incremental updates
+
+As soon as the computation started, it began to randomly add and remove edges, one addition and removal per second. These updates depend on the initial computation, and so we can't report their results until after reporting those of the first round. But, just after the first round emerges, we see outputs like:
+
+```
+observed at (Root, 3):
+elapsed: 261.10050941698137s
+	(23, 1)
+	(24, 2)
+	(25, -2)
+	(27, -1)
+observed at (Root, 4):
+elapsed: 260.100518569001s
+	(21, 1)
+	(24, -1)
+observed at (Root, 5):
+elapsed: 259.1005240849918s
+	(23, 1)
+observed at (Root, 8):
+elapsed: 256.1005283939885s
+	(24, -1)
+	(25, -1)
+observed at (Root, 11):
+elapsed: 253.109466711001s
+	(25, 1)
+	(26, -1)
+```
+
+Each of these outputs reports a change in the numbers of nodes at each distance from the roots. If accumulated with prior counts (the initial counts above, plus any preceding changes), they are the correct counts for the number of nodes *now* at each distance from the roots.
+
+These outputs corresponding to inputs introduced at seconds 3, 4, 5, 8, and 11 from the start of the computation, times at which we haven't even finished introducing the initial 200M randomly generated graph edges. As a consequence, these outputs have fairly large `elapsed` measurements.
+
+However, you may notice that the times at which they are reported, their second of introduction plus the elapsed measurement, are quite tightly concentrated, at roughly 264 seconds. While these results are not available until after the outputs for the first epoch are complete, they do emerge almost immediately thereafter.
+
+### Streaming incremental updates
+
+If we scan through the output until epoch 265, the first one unblocked by the initial 264.1s of work, we see that its elapsed time is quite small. As are those that follow it:
+
+```
+observed at (Root, 265):
+elapsed: 0.0018802949925884604s
+	(25, -1)
+	(26, -1)
+	(27, -1)
+observed at (Root, 268):
+elapsed: 0.0018262710073031485s
+	(24, 1)
+observed at (Root, 273):
+elapsed: 0.0018914630054496229s
+	(25, -1)
+observed at (Root, 274):
+elapsed: 0.00016224500723183155s
+	(22, -1)
+	(24, 1)
+	(29, 1)
+...
+```
+The `elapsed` measurements from this point on are single-digit milliseconds, or less.
+
+## Data parallelism
+
+Differential dataflow is built on [timely dataflow](https://github.com/frankmcsherry/timely-dataflow), a distributed data-parallel dataflow platform. Consequently, it distributes over multiple threads, processes, and computers. The additional resources allow larger computations to be processed more efficiently, but may limit the minimal latency for small updates, as more computations must coordinate.
--- examples/bfs.rs
+++ examples/bfs.rs
@@ -4,7 +4,6 @@ extern crate columnar;
 extern crate timely;
 extern crate differential_dataflow;
 
-use std::hash::Hash;
 use timely::example_shared::*;
 use timely::example_shared::operators::*;
 use timely::communication::ThreadCommunicator;
@@ -17,74 +16,79 @@ use differential_dataflow::collection_trace::LeastUpperBound;
 use differential_dataflow::operators::*;
 
 fn main() {
+    let start = time::precise_time_s();
 
-        let start = time::precise_time_s();
-        let start2 = start.clone();
-        let mut computation = GraphRoot::new(ThreadCommunicator);
+    // define a new computational scope, in which to run BFS
+    let mut computation = GraphRoot::new(ThreadCommunicator);
 
-        let (mut roots, mut graph) = computation.subcomputation(|builder| {
+    // define BFS dataflow; return handles to roots and edges inputs
+    let (mut roots, mut graph) = computation.subcomputation(|builder| {
 
-            let (edge_input, graph) = builder.new_input();
-            let (node_input, roots) = builder.new_input();
+        let (edge_input, graph) = builder.new_input();
+        let (node_input, roots) = builder.new_input();
 
-            // TODO : Type inference falls down w/o the u32, I guess because default int stuffs.
-            bfs::<_,u32>(&graph, &roots).map(|((_,s),w)| (s,w))
-                               .consolidate(|x| *x, |x| *x)
-                               .inspect_batch(move |t, x| {
-                                   let elapsed = time::precise_time_s() - start2;
-                                   println!("{}s:\tobserved at {:?}: {:?} changes; elapsed: {}s", elapsed, t, x.len(), elapsed - (t.inner as f64));
-                                //    for y in x {
-                                //        println!("\t{:?}", y);
-                                //    }
-                               });
+        let dists = bfs(&graph, &roots);    // determine distances to each graph node
 
-            (node_input, edge_input)
-        });
+        dists.map(|((_,s),w)| (s,w))        // keep only the distances, not node ids
+             .consolidate(|x| *x, |x| *x)   // aggregate into one record per distance
+             .inspect_batch(move |t, x| {   // print up something neat for each update
+                 println!("observed at {:?}:", t);
+                 println!("elapsed: {}s", time::precise_time_s() - (start + t.inner as f64));
+                 for y in x {
+                     println!("\t{:?}", y);
+                 }
+             });
 
-        let nodes = 100_000_000;
-        let edge_count = 500_000_000;
+        (node_input, edge_input)
+    });
 
-        let seed: &[_] = &[1, 2, 3, 4];
-        let mut rng1: StdRng = SeedableRng::from_seed(seed);
-        let mut rng2: StdRng = SeedableRng::from_seed(seed);
+    let nodes = 100_000_000u32; // the u32 helps type inference understand what nodes are
+    let edges = 200_000_000;
 
-        println!("performing BFS on {} nodes, {} edges:", nodes, edge_count);
+    let seed: &[_] = &[1, 2, 3, 4];
+    let mut rng1: StdRng = SeedableRng::from_seed(seed);    // rng for edge additions
+    let mut rng2: StdRng = SeedableRng::from_seed(seed);    // rng for edge deletions
 
-        let mut left = edge_count;
-        while left > 0 {
-            let next = if left < 1000 { left } else { 1000 };
-            graph.send_at(0, (0..next).map(|_| ((rng1.gen_range(0, nodes), rng1.gen_range(0, nodes)), 1)));
-            computation.step();
-            left -= next;
+    println!("performing BFS on {} nodes, {} edges:", nodes, edges);
+
+    // trickle edges in to dataflow
+    let mut left = edges;
+    while left > 0 {
+        let next = std::cmp::min(left, 1000);
+        graph.send_at(0, (0..next).map(|_| ((rng1.gen_range(0, nodes),
+                                             rng1.gen_range(0, nodes)), 1)));
+        computation.step();
+        left -= next;
+    }
+
+    // start the root set out with roots 0, 1, and 2
+    roots.send_at(0, vec![(0, 1), (1, 1), (2, 1)].into_iter());
+    roots.advance_to(1);
+    roots.close();
+
+    // repeatedly change edges
+    let mut round = 0 as u32;
+    while computation.step() {
+        // once each full second ticks, change an edge
+        if time::precise_time_s() - start >= round as f64 {
+            // add edges using prior rng; remove edges using fresh rng with the same seed
+            let changes = vec![((rng1.gen_range(0, nodes), rng1.gen_range(0, nodes)), 1),
+                               ((rng2.gen_range(0, nodes), rng2.gen_range(0, nodes)),-1)];
+            graph.send_at(round, changes.into_iter());
+            graph.advance_to(round + 1);
+            round += 1;
         }
+    }
 
-        roots.send_at(0, vec![(0, 1), (1, 1), (2, 1)].into_iter());
-        roots.advance_to(1);
-        roots.close();
-
-        let mut round = 0 as u32;
-        while computation.step() {
-            if time::precise_time_s() - start >= round as f64 {
-
-                let changes = vec![((rng1.gen_range(0, nodes), rng1.gen_range(0, nodes)), 1),
-                                   ((rng2.gen_range(0, nodes), rng2.gen_range(0, nodes)),-1)];
-                graph.send_at(round, changes.into_iter());
-                graph.advance_to(round + 1);
-                round += 1;
-            }
-        }
-
-        graph.close();
-
-        while computation.step() { }
-        computation.step(); // shut down
-
+    graph.close();                  // seal the source of edges
+    while computation.step() { }    // wind down the computation
 }
 
-// returns triples (n, r, s) indicating node n can be reached from root r in s steps.
-fn bfs<G: GraphBuilder, U: UnsignedInt>(edges: &Stream<G, ((U, U), i32)>,
-                                        roots: &Stream<G, (U, i32)>) -> Stream<G, ((U, u32), i32)>
-where G::Timestamp: LeastUpperBound+Hash {
+// returns pairs (n, s) indicating node n can be reached from a root in s steps.
+fn bfs<G: GraphBuilder, U>(edges: &Stream<G, ((U, U), i32)>, roots: &Stream<G, (U, i32)>)
+    -> Stream<G, ((U, u32), i32)>
+where G::Timestamp: LeastUpperBound,
+      U: UnsignedInt {
 
     // initialize roots as reaching themselves at distance 0
     let nodes = roots.map(|(x,w)| ((x, 0), w));
@@ -97,6 +101,8 @@ where G::Timestamp: LeastUpperBound+Hash {
 
         inner.join_u(&edges, |l| l, |e| e, |_k,l,d| (*d, l+1))
              .concat(&nodes)
-             .group_by_u(|x| x, |k,v| (*k, *v), |_, mut s, t| t.push((*s.peek().unwrap().0, 1)))
+             .group_by_u(|x| x, |k,v| (*k, *v), |_, mut s, t| {
+                 t.push((*s.peek().unwrap().0, 1));
+             })
      })
 }
