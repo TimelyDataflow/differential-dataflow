601ae231938b154d92d900237a1ef4a61365ef22
Frank McSherry <fmcsherry@me.com>
2015-09-17 10:06:49 +0200

    updated to reflect feedback name change; now with macros

--- examples/dataflog.rs
+++ examples/dataflog.rs
@@ -5,44 +5,30 @@ extern crate time;
 extern crate timely;
 extern crate differential_dataflow;
 
-use std::hash::{Hash, Hasher};
-use std::fmt::Debug;
 use std::io::{BufReader, BufRead};
 use std::fs::File;
 
-use timely::Data;
 use timely::dataflow::*;
 use timely::dataflow::scopes::Child;
 use timely::dataflow::operators::*;
-use timely::dataflow::operators::feedback::{Observer, Helper};
-use timely::dataflow::channels::pushers::Counter;
-use timely::progress::nested::product::Product;
-use timely::progress::nested::Summary::Local;
+use timely::dataflow::operators::feedback::Handle;
 use timely::progress::timestamp::RootTimestamp;
 
+use differential_dataflow::Data;
 use differential_dataflow::operators::*;
 use differential_dataflow::collection::LeastUpperBound;
 
-use fnv::FnvHasher;
-
-fn hash<T: Hash>(x: &T) -> u64 {
-    let mut h: FnvHasher = Default::default();
-    x.hash(&mut h);
-    h.finish()
-}
-
 /// A collection defined by multiple mutually recursive rules.
-pub struct Variable<G: Scope, D: Ord+Default+Clone+Data+Debug+Hash>
+pub struct Variable<G: Scope, D: Default+Data>
 where G::Timestamp: LeastUpperBound {
-    feedback: Option<Helper<Product<G::Timestamp, u64>,(D, i32),Counter<Product<G::Timestamp, u64>,
-                                         (D, i32),Observer<Product<G::Timestamp, u64>,(D, i32)>>>>,
+    feedback: Option<Handle<G::Timestamp, u64,(D, i32)>>,
     current:  Stream<Child<G, u64>, (D,i32)>,
 }
 
-impl<G: Scope, D: Ord+Default+Clone+Data+Debug+Hash> Variable<G, D> where G::Timestamp: LeastUpperBound {
+impl<G: Scope, D: Default+Data> Variable<G, D> where G::Timestamp: LeastUpperBound {
     /// Creates a new `Variable` and a `Stream` representing its output, from a supplied `source` stream.
     pub fn from(source: &Stream<Child<G, u64>, (D,i32)>) -> (Variable<G, D>, Stream<Child<G,u64>, (D, i32)>) {
-        let (feedback, cycle) = source.scope().loop_variable(Product::new(G::Timestamp::max(), u64::max_value()), Local(1));
+        let (feedback, cycle) = source.scope().loop_variable(u64::max_value(), 1);
         let mut result = Variable { feedback: Some(feedback), current: cycle.clone() };
         let stream = cycle.clone();
         result.add(source);
@@ -54,10 +40,10 @@ impl<G: Scope, D: Ord+Default+Clone+Data+Debug+Hash> Variable<G, D> where G::Tim
     }
 }
 
-impl<G: Scope, D: Ord+Default+Debug+Hash+Clone+Data> Drop for Variable<G, D> where G::Timestamp: LeastUpperBound {
+impl<G: Scope, D: Default+Data> Drop for Variable<G, D> where G::Timestamp: LeastUpperBound {
     fn drop(&mut self) {
         if let Some(feedback) = self.feedback.take() {
-            self.current.group_by(|x| (x, ()), hash, hash, |x,_| x.clone(), |x,_,t| t.push((x.clone(),1)))
+            self.current.group_by(|x| (x, ()), |x| x.hashed(), |x| x.hashed(), |x,_| x.clone(), |_,_,t| t.push(((),1)))
                         .connect_loop(feedback);
         }
     }
@@ -70,11 +56,37 @@ macro_rules! rule {
                 &$name3.0,
                 |($( $var2, )*)| (($( $var4, )*), ( $($var2, )*)),
                 |($( $var3, )*)| (($( $var5, )*), ( $($var3, )*)),
-                hash,
+                |x| x.hashed(),
                 |_, &($( $var2, )*), &($( $var3, )*)| (($( $var2, )*), ($( $var3, )*)));
         $name1.1.add(&result.map(|((($( $var2, )*), ($( $var3, )*)), __w)| (($( $var1, )*), __w)));
 
-        let temp = result.semijoin_by(&$name1.2, |(($( $var2, )*), ($( $var3, )*))| (($( $var1, )*), (($( $var2, )*), ($( $var3, )*))), hash, |_, &(($( $var2, )*), ($( $var3, )*))| (($( $var2, )*), ($( $var3, )*)));
+        let temp = result.semijoin_by(
+            &$name1.2,
+            |(($( $var2, )*), ($( $var3, )*))| (($( $var1, )*), (($( $var2, )*), ($( $var3, )*))),
+            |x| x.hashed(),
+            |_, &(($( $var2, )*), ($( $var3, )*))| (($( $var2, )*), ($( $var3, )*)));
+        $name2.3.add(&temp.map(|(( ($( $var2, )*) ,_),__w)| (($( $var2, )*),__w)));
+        $name3.3.add(&temp.map(|(( _, ($( $var3, )*)),__w)| (($( $var3, )*),__w)));
+
+        temp
+    }};
+}
+
+macro_rules! rule_u {
+    ($name1: ident ($($var1:ident),*) := $name2: ident ($($var2:ident),*) $name3: ident ($($var3:ident),*) : $var4:ident = $var5:ident) => {{
+        let result =
+            $name2.0.join_by_u(
+                &$name3.0,
+                |($( $var2, )*)| ($var4, ( $($var2, )*)),
+                |($( $var3, )*)| ($var5, ( $($var3, )*)),
+                |_, &($( $var2, )*), &($( $var3, )*)| (($( $var2, )*), ($( $var3, )*)));
+        $name1.1.add(&result.map(|((($( $var2, )*), ($( $var3, )*)), __w)| (($( $var1, )*), __w)));
+
+        let temp = result.semijoin_by(
+            &$name1.2,
+            |(($( $var2, )*), ($( $var3, )*))| (($( $var1, )*), (($( $var2, )*), ($( $var3, )*))),
+            |x| x.hashed(),
+            |_, &(($( $var2, )*), ($( $var3, )*))| (($( $var2, )*), ($( $var3, )*)));
         $name2.3.add(&temp.map(|(( ($( $var2, )*) ,_),__w)| (($( $var2, )*),__w)));
         $name3.3.add(&temp.map(|(( _, ($( $var3, )*)),__w)| (($( $var3, )*),__w)));
 
@@ -115,25 +127,25 @@ fn main() {
                 let mut p = variable!(inner : p, p_query);
                 let mut q = variable!(inner : q, q_query);
 
-                let ir1 = rule!(p(x,z)   := p(x,_y1) p(_y2,z)   : (_y1) = (_y2));
-                let ir2 = rule!(q(x,r,z) := p(x,_y1) q(_y2,r,z) : (_y1) = (_y2));
+                let ir1 = rule_u!(p(x,z)   := p(x,_y1) p(_y2,z)   : _y1 = _y2);
+                let ir2 = rule_u!(q(x,r,z) := p(x,_y1) q(_y2,r,z) : _y1 = _y2);
 
                 // P(x,z) := P(y,w), Q(x,r,y), U(w,r,z)
                 let ir3 = p.0.join_by_u(&q.0, |(y,w)| (y,w), |(x,r,y)| (y,(x,r)), |&y, &w, &(x,r)| (r,w,x,y))
-                             .join_by(&u, |(r,w,x,y)| ((r,w), (y,x)), |(w,r,z)| ((r,w),z), hash, |&(r,w), &(y,x), &z| (r,w,x,y,z));
+                             .join_by(&u, |(r,w,x,y)| ((r,w), (y,x)), |(w,r,z)| ((r,w),z), |x| x.hashed(), |&(r,w), &(y,x), &z| (r,w,x,y,z));
                 p.1.add(&ir3.map(|((_,_,x,_,z),w)| ((x,z),w)));
-                let ir3_need = ir3.semijoin_by(&p.2, |(r,w,x,y,z)| ((x,z), (r,w,y)), hash, |&(x,z),&(r,w,y)| (r,w,x,y,z));
+                let ir3_need = ir3.semijoin_by(&p.2, |(r,w,x,y,z)| ((x,z), (r,w,y)), |x| x.hashed(), |&(x,z),&(r,w,y)| (r,w,x,y,z));
                 p.3.add(&ir3_need.map(|((_,w,_,y,_),w2)| ((y,w),w2)));
                 q.3.add(&ir3_need.map(|((r,_,x,y,_),w2)| ((x,r,y),w2)));
 
                 // extract the results and return
-                (p.2.leave(), q.2.leave(), ir1.leave(), ir2.leave(), ir3.leave())
+                (p.2.leave(), q.2.leave(), ir1.leave(), ir2.leave(), ir3_need.leave())
             });
 
             let (probe, _) = p_base.consolidate().probe();
 
-            p_base.consolidate().inspect(|&(x,_w)| println!("Required P{:?}", x));
-            q_base.consolidate().inspect(|&(x,_w)| println!("Required Q{:?}", x));
+            // p_base.consolidate().inspect(|&(x,_w)| println!("Required P{:?}", x));
+            // q_base.consolidate().inspect(|&(x,_w)| println!("Required Q{:?}", x));
 
             ir1.consolidate().inspect(|&(x,_w)| println!("Required IR1 {:?}", x));
             ir2.consolidate().inspect(|&(x,_w)| println!("Required IR2 {:?}", x));
@@ -149,7 +161,9 @@ fn main() {
                 let elts: Vec<&str> = line[..].split(",").collect();
                 let src: u32 = elts[0].parse().ok().expect("malformed src");
                 let dst: u32 = elts[1].parse().ok().expect("malformed dst");
-                p.send(((src, dst), 1));
+                if src != dst {
+                    p.send(((src, dst), 1));
+                }
             }
 
             let q_file = BufReader::new(File::open("/Users/mcsherry/Desktop/q.txt").unwrap());
@@ -185,6 +199,7 @@ fn main() {
         while probe.lt(&RootTimestamp::new(1)) {
             root.step();
         }
+        root.step();
 
         println!("derivation:\t{}", time::precise_time_s() - start);
         let timer = time::precise_time_s();
