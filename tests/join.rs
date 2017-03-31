cc23ff08f8497cab0543640494e3f669a688eeb8
Frank McSherry <fmcsherry@me.com>
2017-03-31 12:36:00 +0200

    added scaling tests which should time out on non-linear implementations

--- tests/join.rs
+++ tests/join.rs
@@ -1,10 +1,11 @@
 extern crate timely;
 extern crate differential_dataflow;
 
-use timely::dataflow::operators::{ToStream, Capture};
+use timely::progress::timestamp::RootTimestamp;
+use timely::dataflow::operators::{ToStream, Capture, Map};
 use timely::dataflow::operators::capture::Extract;
 use differential_dataflow::AsCollection;
-use differential_dataflow::operators::{Consolidate, Join};
+use differential_dataflow::operators::{Consolidate, Join, Count};
 
 #[test]
 fn join() {
@@ -74,4 +75,29 @@ fn antijoin() {
     let extracted = data.extract();
     assert_eq!(extracted.len(), 1);
     assert_eq!(extracted[0].1, vec![((1,2), Default::default(),1)]);
+}
+
+#[test] fn join_scale_100() { join_scaling(100); }
+#[test] fn join_scale_1000() { join_scaling(1000); }
+#[test] fn join_scale_10000() { join_scaling(10000); }
+#[test] fn join_scale_100000() { join_scaling(100000); }
+#[test] fn join_scale_1000000() { join_scaling(1000000); }
+
+fn join_scaling(scale: u64) {
+
+    let data = timely::example(move |scope| {
+
+        let counts = (0 .. 1).to_stream(scope)
+                             .flat_map(move |_| (0..scale).map(|i| ((), RootTimestamp::new(i), 1)))
+                             .as_collection()
+                             .count();
+
+        let odds = counts.filter(|x| x.1 % 2 == 1);
+        let evens = counts.filter(|x| x.1 % 2 == 0);
+
+        odds.join(&evens).inner.capture()
+    });
+
+    let extracted = data.extract();
+    assert_eq!(extracted.len(), 0);
 }
