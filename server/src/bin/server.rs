803972f2f9e7504d3b9c9512012ac8916ef9f7e7
Frank McSherry <fmcsherry@me.com>
2017-11-24 09:42:53 -0500

    update server project

--- server/src/bin/server.rs
+++ server/src/bin/server.rs
@@ -8,6 +8,7 @@ static ALLOC: System = System;
 
 extern crate libloading;
 
+extern crate timely_communication;
 extern crate timely;
 extern crate dd_server;
 
@@ -16,12 +17,21 @@ use std::io::Write;
 
 use std::rc::Rc;
 use std::cell::RefCell;
-use std::sync::{Arc, Mutex};
+use std::sync::{Arc, Weak, Mutex};
+
+use std::collections::VecDeque;
+use std::sync::mpsc::Receiver;
+
+use timely_communication::Allocate;
 
 use timely::PartialOrder;
+use timely::progress::timestamp::RootTimestamp;
+use timely::progress::nested::product::Product;
+use timely::dataflow::scopes::root::Root;
 use timely::dataflow::operators::Unary;
 use timely::dataflow::channels::pact::Exchange;
 use timely::dataflow::operators::Probe;
+use timely::dataflow::operators::probe::Handle as ProbeHandle;
 use timely::dataflow::operators::generic::source;
 
 use libloading::{Library, Symbol};
@@ -33,89 +43,30 @@ fn main() {
     // shared queue of commands to serialize (in the "put in an order" sense).
     let (send, recv) = std::sync::mpsc::channel();
     let recv = Arc::new(Mutex::new(recv));
+    let weak = Arc::downgrade(&recv);
 
     // demonstrate dynamic loading of dataflows via shared libraries.
     let guards = timely::execute_from_args(std::env::args(), move |worker| {
 
-        let recv = recv.clone();
         let timer = ::std::time::Instant::now();
 
         // map from string name to arranged graph.
-        let mut handles = TraceHandler::new();//HashMap::new();
+        let mut handles = TraceHandler::new();
 
         // common probe used by all dataflows to express progress information.
         let mut probe = timely::dataflow::operators::probe::Handle::new();
-        let mut probe2 = probe.clone();
 
-        let command_queue = std::collections::VecDeque::new();
-        let command_queue1 = Rc::new(RefCell::new(command_queue));
-        let command_queue2 = command_queue1.clone();
+        // queue shared between serializer (producer) and command loop (consumer).
+        let command_queue_strong = Rc::new(RefCell::new(VecDeque::new()));
+        build_command_serializer(worker, timer, weak.clone(), &command_queue_strong, &mut probe);
+        let command_queue = Rc::downgrade(&command_queue_strong);
+        drop(command_queue_strong);
 
-        // build a dataflow used to serialize and circulate commands
-        worker.dataflow(move |dataflow| {
+        // continue running as long as we haven't dropped the queue.
+        while let Some(command_queue) = command_queue.upgrade() {
 
-            let peers = dataflow.peers();
-
-            let mut recvd = Vec::new();
-
-            // a source that attempts to pull from `recv` and produce commands for everyone
-            source(dataflow, "InputCommands", move |mut capability| {
-
-                // closure broadcasts any commands it grabs.
-                move |output| {
-
-                    // determine current nanoseconds
-                    let elapsed = timer.elapsed();
-                    let ns = (elapsed.as_secs() * 1000000000 + elapsed.subsec_nanos() as u64) as usize;
-
-                    // this could be less frequent if needed.
-                    let mut time = capability.time().clone();
-                    time.inner = ns;
-
-                    capability.downgrade(&time);
-
-                    if let Ok(recv) = recv.try_lock() {
-                        while let Ok(command) = recv.try_recv() {
-                            let command: Vec<String> = command;
-                            let mut session = output.session(&capability);
-                            for worker_index in 0 .. peers {
-                                session.give((worker_index, command.clone()));
-                            }
-                        }
-                    }
-                }
-            })
-            .unary_notify(
-                Exchange::new(|x: &(usize, Vec<String>)| x.0 as u64), 
-                "InputCommandsRecv", 
-                Vec::new(), 
-                move |input, output, notificator| {
-
-                // grab each command and queue it up
-                input.for_each(|time, data| {
-                    recvd.extend(data.drain(..).map(|(_,command)| (time.time().clone(), command)));
-                    if false { output.session(&time).give(0u64); }
-                });
-
-                recvd.sort();
-
-                // try to move any commands at completed times to a shared queue.
-                if let Ok(mut borrow) = command_queue1.try_borrow_mut() {
-                    while recvd.len() > 0 && !notificator.frontier(0).iter().any(|x| x.less_than(&recvd[0].0)) {
-                        borrow.push_back(recvd.remove(0));
-                    }
-                }
-                else { panic!("failed to borrow shared command queue"); }
-
-            })
-            .probe_with(&mut probe2);
-
-        });
-
-        loop {
-
-            if let Ok(mut borrow) = command_queue2.try_borrow_mut() {
-                while let Some((_time, mut command)) = borrow.pop_front() {
+            if let Ok(mut borrow) = command_queue.try_borrow_mut() {
+                while let Some(mut command) = borrow.pop_front() {
 
                     let index = worker.index();
                     println!("worker {:?}: received command: {:?}", index, command);
@@ -167,31 +118,125 @@ fn main() {
             // than providing no guaranteed progress for e.g. iterative computations.
 
             worker.step();
-            std::thread::yield_now();   // so that over-subscribed worker counts still feel interactive
+            // std::thread::yield_now();   // so that over-subscribed worker counts still feel interactive
         }
+
+        println!("worker {}: command queue unavailable; exiting command loop.", worker.index());
     });
 
     // the main thread now continues, to read from the console and issue work to the shared queue.
 
     std::io::stdout().flush().unwrap();
     let input = std::io::stdin();
-    for line in input.lock().lines().map(|x| x.unwrap()) {
-        let elts: Vec<_> = line.split_whitespace().map(|x| x.to_owned()).collect();
 
-        if elts.len() > 0 {
-            match elts[0].as_str() {
-                "help" => { println!("valid commands are currently: bind, drop, exit, help, list, load"); },
-                "bind" => { println!("ideally this would load and bind a library to some delightful name"); },
-                "drop" => { send.send(elts).expect("failed to send command"); }
-                "exit" => { println!("gotta ^C, sorry."); },
-                "load" => { send.send(elts).expect("failed to send command"); },
-                "list" => { println!("this is where we would list loaded collections"); },
-                _ => { println!("unrecognized command: {:?}", elts[0]); },
+    let mut done = false;
+
+    while !done {
+
+        if let Some(line) = input.lock().lines().map(|x| x.unwrap()).next() {
+            let elts: Vec<_> = line.split_whitespace().map(|x| x.to_owned()).collect();
+
+            if elts.len() > 0 {
+                match elts[0].as_str() {
+                    "help" => { println!("valid commands are currently: bind, drop, exit, help, list, load"); },
+                    "bind" => { println!("ideally this would load and bind a library to some delightful name"); },
+                    "drop" => { send.send(elts).expect("failed to send command"); }
+                    "exit" => { done = true; },
+                    "load" => { send.send(elts).expect("failed to send command"); },
+                    "list" => { println!("this is where we would list loaded collections"); },
+                    _ => { println!("unrecognized command: {:?}", elts[0]); },
+                }
             }
-        }
 
-        std::io::stdout().flush().unwrap();
+            std::io::stdout().flush().unwrap();
+        }
     }
 
+    println!("main: exited command loop");
+    drop(send); // workers will only terminate if they know the input command queue is closed.
+    drop(recv);
+
     guards.unwrap();
+}
+
+fn build_command_serializer<A: Allocate>(
+    worker: &mut Root<A>,
+    timer: ::std::time::Instant,
+    input: Weak<Mutex<Receiver<Vec<String>>>>,
+    target: &Rc<RefCell<VecDeque<Vec<String>>>>,
+    handle: &mut ProbeHandle<Product<RootTimestamp, usize>>,
+) {
+
+    let target = target.clone();
+
+    // build a dataflow used to serialize and circulate commands
+    worker.dataflow(move |dataflow| {
+
+        let peers = dataflow.peers();
+        let mut recvd = Vec::new();
+
+        // a source that attempts to pull from `recv` and produce commands for everyone
+        source(dataflow, "InputCommands", move |capability| {
+
+            // so we can drop, if input queue vanishes.
+            let mut capability = Some(capability);
+
+            // closure broadcasts any commands it grabs.
+            move |output| {
+
+                if let Some(input) = input.upgrade() {
+
+                    // determine current nanoseconds
+                    if let Some(capability) = capability.as_mut() {
+
+                        // this could be less frequent if needed.
+                        let mut time = capability.time().clone();
+                        let elapsed = timer.elapsed();
+                        time.inner = (elapsed.as_secs() * 1000000000 + elapsed.subsec_nanos() as u64) as usize;
+
+                        // downgrade the capability.
+                        capability.downgrade(&time);
+
+                        if let Ok(input) = input.try_lock() {
+                            while let Ok(command) = input.try_recv() {
+                                let command: Vec<String> = command;
+                                let mut session = output.session(&capability);
+                                for worker_index in 0 .. peers {
+                                    session.give((worker_index, command.clone()));
+                                }
+                            }
+                        }
+                    }
+                    else { panic!("command serializer: capability lost while input queue valid"); }
+                }
+                else {
+                    capability = None;
+                }
+            }
+        })
+        .unary_notify(
+            Exchange::new(|x: &(usize, Vec<String>)| x.0 as u64), 
+            "InputCommandsRecv", 
+            Vec::new(), 
+            move |input, output, notificator| {
+
+            // grab each command and queue it up
+            input.for_each(|time, data| {
+                recvd.extend(data.drain(..).map(|(_,command)| (time.time().clone(), command)));
+                if false { output.session(&time).give(0u64); }
+            });
+
+            recvd.sort();
+
+            // try to move any commands at completed times to a shared queue.
+            if let Ok(mut borrow) = target.try_borrow_mut() {
+                while recvd.len() > 0 && !notificator.frontier(0).iter().any(|x| x.less_than(&recvd[0].0)) {
+                    borrow.push_back(recvd.remove(0).1);
+                }
+            }
+            else { panic!("failed to borrow shared command queue"); }
+
+        })
+        .probe_with(handle);
+    });
 }
