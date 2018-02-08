426f149e3720f0ea0151d368df86f13b44ba3b2d
Frank McSherry <fmcsherry@me.com>
2018-02-08 21:22:30 +0100

    more featurez

--- examples/graphs.rs
+++ examples/graphs.rs
@@ -12,6 +12,8 @@ use differential_dataflow::Collection;
 use differential_dataflow::operators::*;
 use differential_dataflow::lattice::Lattice;
 use differential_dataflow::operators::iterate::Variable;
+use differential_dataflow::operators::arrange::ArrangeByKey;
+use differential_dataflow::operators::arrange::ArrangeBySelf;
 
 type Node = usize;
 type Edge = (Node, Node);
@@ -20,6 +22,7 @@ fn main() {
 
     let nodes: usize = std::env::args().nth(1).unwrap().parse().unwrap();
     let edges: usize = std::env::args().nth(2).unwrap().parse().unwrap();
+    let batch: usize = std::env::args().nth(3).unwrap().parse().unwrap();
     let inspect: bool = std::env::args().any(|x| x == "inspect");
 
     // Our setting involves four read query types, and two updatable base relations.
@@ -51,35 +54,38 @@ fn main() {
             let (state_input, state) = scope.new_collection();
             let (graph_input, graph) = scope.new_collection();
 
+            let state_indexed = state.arrange_by_key();
+            let graph_indexed = graph.map(|(src, dst)| (dst, src))
+                                     .concat(&graph)
+                                     .arrange_by_key();
+
             // Q1: Point lookups on `state`:
-            state
-                .semijoin(&q1)
+            q1  .arrange_by_self()
+                .join_core(&state_indexed, |&query, &(), &state| Some((query, state)))
                 .filter(move |_| inspect)
                 .inspect(|x| println!("Q1: {:?}", x))
                 .probe_with(&mut probe);
 
             // Q2: One-hop lookups on `state`:
-            graph
-                .semijoin(&q2)
-                .map(|(query, friend)| (friend, query))
-                .join_map(&state, |_friend, &query, &state| (query, state))
+            q2  .arrange_by_self()
+                .join_core(&graph_indexed, |&query, &(), &friend| Some((friend, query)))
+                .join_core(&state_indexed, |_friend, &query, &state| Some((query, state)))
                 .filter(move |_| inspect)
                 .inspect(|x| println!("Q2: {:?}", x))
                 .probe_with(&mut probe);
 
             // Q3: Two-hop lookups on `state`:
-            graph
-                .semijoin(&q3)
-                .map(|(query, friend)| (friend, query))
-                .join_map(&graph, |_friend, &query, &friend2| (friend2, query))
-                .join_map(&state, |_friend2, &query, &state| (query, state))
+            q3  .arrange_by_self()
+                .join_core(&graph_indexed, |&query, &(), &friend| Some((friend, query)))
+                .join_core(&graph_indexed, |_friend, &query, &friend2| Some((friend2, query)))
+                .join_core(&state_indexed, |_friend2, &query, &state| Some((query, state)))
                 .filter(move |_| inspect)
                 .consolidate()
                 .inspect(|x| println!("Q3: {:?}", x))
                 .probe_with(&mut probe);
 
             // Q4: Shortest path queries:
-            bidijkstra(&graph, &q4)
+            bidijkstra(&graph_indexed, &graph_indexed, &q4)
                 .filter(move |_| inspect)
                 .inspect(|x| println!("Q4: {:?}", x))
                 .probe_with(&mut probe);
@@ -92,7 +98,7 @@ fn main() {
         // let mut rng2: StdRng = SeedableRng::from_seed(seed);    // rng for edge deletions
         let mut rng3: StdRng = SeedableRng::from_seed(seed);    // rng for queries
 
-        println!("performing workload on random graph with {} nodes, {} edges:", nodes, edges);
+        if index == 0 { println!("performing workload on random graph with {} nodes, {} edges:", nodes, edges); }
 
         let worker_edges = edges/peers + if index < (edges % peers) { 1 } else { 0 };
         for _ in 0 .. worker_edges {
@@ -114,60 +120,79 @@ fn main() {
         // finish graph loading work.
         while probe.less_than(q1.time()) { worker.step(); }
 
-        println!("{:?}\tgraph loaded", timer.elapsed());
+        if index == 0 { println!("{:?}\tgraph loaded", timer.elapsed()); }
+
+        let worker_batch = batch/peers + if index < batch % peers { 1 } else { 0 };
 
         // Q1 testing:
         let timer_q1 = ::std::time::Instant::now();
         for round in 1 .. 1001 {
-            q1.insert(rng3.gen_range(0, nodes));
+            for _ in 0 .. worker_batch {
+                q1.insert(rng3.gen_range(0, nodes));
+            }
             q1.advance_to(round);
             q1.flush();
             while probe.less_than(q1.time()) { worker.step(); }
         }
-        println!("{:?}\tq1 eval complete; avg: {:?}", timer.elapsed(), timer_q1.elapsed()/1000);
+        if index == 0 { println!("{:?}\tq1 eval complete; avg: {:?}", timer.elapsed(), timer_q1.elapsed()/1000); }
         q1.close();
 
         // Q2 testing:
         let timer_q2 = ::std::time::Instant::now();
         for round in 1001 .. 2001 {
-            q2.insert(rng3.gen_range(0, nodes));
+            for _ in 0 .. worker_batch {
+                q2.insert(rng3.gen_range(0, nodes));
+            }
             q2.advance_to(round);
             q2.flush();
             while probe.less_than(q2.time()) { worker.step(); }
         }
-        println!("{:?}\tq2 eval complete; avg: {:?}", timer.elapsed(), timer_q2.elapsed()/1000);
+        if index == 0 { println!("{:?}\tq2 eval complete; avg: {:?}", timer.elapsed(), timer_q2.elapsed()/1000); }
         q2.close();
 
         // Q3 testing:
         let timer_q3 = ::std::time::Instant::now();
         for round in 2001 .. 3001 {
-            q3.insert(rng3.gen_range(0, nodes));
+            for _ in 0 .. worker_batch {
+                q3.insert(rng3.gen_range(0, nodes));
+            }
             q3.advance_to(round);
             q3.flush();
             while probe.less_than(q3.time()) { worker.step(); }
         }
-        println!("{:?}\tq3 eval complete; avg: {:?}", timer.elapsed(), timer_q3.elapsed()/1000);
+        if index == 0 { println!("{:?}\tq3 eval complete; avg: {:?}", timer.elapsed(), timer_q3.elapsed()/1000); }
         q3.close();
 
         // Q4 testing:
         let timer_q4 = ::std::time::Instant::now();
         for round in 3001 .. 4001 {
-            q4.insert((rng3.gen_range(0, nodes), rng3.gen_range(0, nodes)));
+            for _ in 0 .. worker_batch {
+               q4.insert((rng3.gen_range(0, nodes), rng3.gen_range(0, nodes)));
+            }
             q4.advance_to(round);
             q4.flush();
             while probe.less_than(q4.time()) { worker.step(); }
         }
-        println!("{:?}\tq4 eval complete; avg: {:?}", timer.elapsed(), timer_q4.elapsed()/1000);
+        if index == 0 { println!("{:?}\tq4 eval complete; avg: {:?}", timer.elapsed(), timer_q4.elapsed()/1000); }
         q4.close();
 
     }).unwrap();
 }
 
+use differential_dataflow::trace::implementations::ord::OrdValSpine as DefaultValTrace;
+use differential_dataflow::operators::arrange::TraceAgent;
+use differential_dataflow::operators::arrange::Arranged;
+
+type Arrange<G: Scope, K, V, R> = Arranged<G, K, V, R, TraceAgent<K, V, G::Timestamp, R, DefaultValTrace<K, V, G::Timestamp, R>>>;
+
 // returns pairs (n, s) indicating node n can be reached from a root in s steps.
-fn bidijkstra<G: Scope>(edges: &Collection<G, Edge>, goals: &Collection<G, (Node, Node)>) -> Collection<G, ((Node, Node), u32)>
+fn bidijkstra<G: Scope>(
+    forward_graph: &Arrange<G, Node, Node, isize>,
+    reverse_graph: &Arrange<G, Node, Node, isize>,
+    goals: &Collection<G, (Node, Node)>) -> Collection<G, ((Node, Node), u32)>
 where G::Timestamp: Lattice+Ord {
 
-    edges.scope().scoped(|inner| {
+    goals.scope().scoped(|inner| {
 
         // Our plan is to start evolving distances from both sources and destinations. 
         // The evolution from a source or destination should continue as long as there
@@ -178,7 +203,8 @@ where G::Timestamp: Lattice+Ord {
         let reverse = Variable::from(goals.map(|(_,y)| (y,(y,0))).enter(inner));
 
         let goals = goals.enter(inner);
-        let edges = edges.enter(inner);
+        let forward_graph = forward_graph.enter(inner);
+        let reverse_graph = reverse_graph.enter(inner);
 
         // Let's determine which (src, dst) pairs are ready to return.
         //
@@ -205,7 +231,8 @@ where G::Timestamp: Lattice+Ord {
             .map(|(med, (src, dist))| (src, (med, dist)))
             .semijoin(&forward_active)
             .map(|(src, (med, dist))| (med, (src, dist)))
-            .join_map(&edges, |_med, &(src, dist), &next| (next, (src, dist+1)))
+            .join_core(&forward_graph, |_med, &(src, dist), &next| Some((next, (src, dist+1))))
+            // .join_map(&edges, |_med, &(src, dist), &next| (next, (src, dist+1)))
             .concat(&forward)
             .map(|(next, (src, dist))| ((next, src), dist))
             .group(|_key, s, t| t.push((*s[0].0, 1)))
@@ -220,7 +247,8 @@ where G::Timestamp: Lattice+Ord {
             .map(|(med, (rev, dist))| (rev, (med, dist)))
             .semijoin(&reverse_active)
             .map(|(rev, (med, dist))| (med, (rev, dist)))
-            .join_map(&edges.map(|(x,y)| (y,x)), |_med, &(rev, dist), &next| (next, (rev, dist+1)))
+            .join_core(&reverse_graph, |_med, &(rev, dist), &next| Some((next, (rev, dist+1))))
+            // .join_map(&edges.map(|(x,y)| (y,x)), |_med, &(rev, dist), &next| (next, (rev, dist+1)))
             .concat(&reverse)
             .map(|(next, (rev, dist))| ((next, rev), dist))
             .group(|_key, s, t| t.push((*s[0].0, 1)))
