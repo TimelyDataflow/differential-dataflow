dc91f4c62b68ef4662eacc5d1afedb38afde2631
Frank McSherry <fmcsherry@me.com>
2018-02-14 16:07:06 +0100

    prep for 0.5

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,17 @@
 //! Differential dataflow is a high-throughput, low-latency data-parallel programming framework.
 //!
 //! Differential dataflow programs are written in a collection-oriented style, where you transform
-//! collections of records using traditional operations like `map`, `filter`, `join`, and `group_by`. 
+//! collections of records using traditional operations like `map`, `filter`, `join`, and `group_by`.
 //! Differential dataflow also includes the less traditional operation `iterate`, which allows you
 //! to repeatedly apply differential dataflow transformations to collections.
 //!
-//! Once you have defined a differential dataflow computation, you may then add records to or remove 
-//! records from its inputs; the system will automatically update the computation's outputs with the 
+//! Once you have defined a differential dataflow computation, you may then add records to or remove
+//! records from its inputs; the system will automatically update the computation's outputs with the
 //! appropriate corresponding additions and removals, and report these changes to you.
 //!
-//! Differential dataflow is built on the [timely dataflow](https://github.com/frankmcsherry/timely-dataflow) 
-//! framework for data-parallel programming which automatically parallelizes across multiple threads, 
-//! processes, and computers. Furthermore, because it uses timely dataflow's primitives, it seamlessly 
+//! Differential dataflow is built on the [timely dataflow](https://github.com/frankmcsherry/timely-dataflow)
+//! framework for data-parallel programming which automatically parallelizes across multiple threads,
+//! processes, and computers. Furthermore, because it uses timely dataflow's primitives, it seamlessly
 //! inter-operates with other timely dataflow computations.
 //!
 //! Differential dataflow is still very much a work in progress, with features and ergonomics still
@@ -19,10 +19,10 @@
 //!
 //! # Examples
 //!
-//! This fragment creates a collection of pairs of integers, imagined as graph edges, and then counts 
-//! first the number of times the source coordinate occurs, and then the number of times each count 
+//! This fragment creates a collection of pairs of integers, imagined as graph edges, and then counts
+//! first the number of times the source coordinate occurs, and then the number of times each count
 //! occurs, giving us a sense for the distribution of degrees in the graph.
-//! 
+//!
 //! ```ignore
 //! // create a a degree counting differential dataflow
 //! let (mut input, probe) = worker.dataflow(|scope| {
@@ -45,7 +45,7 @@
 //!     (input, probe)
 //! });
 //! ```
-//! 
+//!
 //! Now assembled, we can drive the computation like a timely dataflow computation, by pushing update
 //! records (triples of data, time, and change in count) at the `input` stream handle. The `probe` is
 //! how timely dataflow tells us that we have seen all corresponding output updates (in case there are
@@ -60,14 +60,14 @@
 //!     }
 //!
 //!     input.flush();
-//!     while probe.less_than(input.time()) { 
-//!        worker.step(); 
+//!     while probe.less_than(input.time()) {
+//!        worker.step();
 //!     }
 //! }
 //! ```
-//! 
+//!
 //! This example should print out the 100 changes in the output, in this case each reflecting the increase
-//! of some node degree by one (typically four output changes, corresponding to the addition and deletion 
+//! of some node degree by one (typically four output changes, corresponding to the addition and deletion
 //! of the new and old counts of the old and new degrees of the affected node).
 
 #![forbid(missing_docs)]
