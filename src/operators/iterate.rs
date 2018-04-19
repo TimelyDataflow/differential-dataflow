de49f0684252df3d5eb2f6710c6d8583786f4963
Frank McSherry <fmcsherry@me.com>
2018-04-19 09:21:49 +0200

    general variable

--- src/operators/iterate.rs
+++ src/operators/iterate.rs
@@ -1,32 +1,32 @@
 //! Iterative application of a differential dataflow fragment.
 //!
-//! The `iterate` operator takes as an argument a closure from a differential dataflow collection 
-//! to a collection of the same type. The output collection is the result of applying this closure 
+//! The `iterate` operator takes as an argument a closure from a differential dataflow collection
+//! to a collection of the same type. The output collection is the result of applying this closure
 //! an unbounded number of times.
 //!
 //! The implementation of `iterate` does not directly apply the closure, but rather establishes an
 //! iterative timely dataflow subcomputation, in which differences circulate until they dissipate
 //! (indicating that the computation has reached fixed point), or until some number of iterations
-//! have passed. 
+//! have passed.
 //!
-//! **Note**: The dataflow assembled by `iterate` does not automatically insert `consolidate` for 
+//! **Note**: The dataflow assembled by `iterate` does not automatically insert `consolidate` for
 //! you. This means that either (i) you should insert one yourself, (ii) you should be certain that
-//! all paths from the input to the output of the loop involve consolidation, or (iii) you should 
+//! all paths from the input to the output of the loop involve consolidation, or (iii) you should
 //! be worried that logically cancelable differences may circulate indefinitely.
 //!
-//! #Details 
+//! #Details
 //!
-//! The `iterate` method is written using a `Variable`, which lets you define your own iterative 
-//! computations when `iterate` itself is not sufficient. This can happen when you have two 
-//! collections that should evolve simultaneously, or when you would like to rotate your loop and 
+//! The `iterate` method is written using a `Variable`, which lets you define your own iterative
+//! computations when `iterate` itself is not sufficient. This can happen when you have two
+//! collections that should evolve simultaneously, or when you would like to rotate your loop and
 //! return an intermediate result.
-//! 
+//!
 //! Using `Variable` requires more explicit arrangement of your computation, but isn't much more
-//! complicated. You must define a new variable from an existing stream (its initial value), and 
+//! complicated. You must define a new variable from an existing stream (its initial value), and
 //! then set it to be a function of this variable (and perhaps other collections and variables).
 //!
 //! A `Variable` dereferences to a `Collection`, the one corresponding to its value in each iteration,
-//! and it can be used in most situations where a collection can be used. The act of setting a 
+//! and it can be used in most situations where a collection can be used. The act of setting a
 //! `Variable` consumes it and returns the corresponding `Collection`, preventing you from setting
 //! it multiple times.
 
@@ -67,7 +67,7 @@ pub trait Iterate<G: Scope, D: Data, R: Diff> {
     ///              });
     ///     });
     /// }
-    /// ```    
+    /// ```
     fn iterate<F>(&self, logic: F) -> Collection<G, D, R>
         where G::Timestamp: Lattice,
               for<'a> F: FnOnce(&Collection<Child<'a, G, u64>, D, R>)->Collection<Child<'a, G, u64>, D, R>;
@@ -96,7 +96,7 @@ impl<G: Scope, D: Ord+Data+Debug, R: Diff> Iterate<G, D, R> for Collection<G, D,
 /// A differential dataflow collection variable
 ///
 /// The `Variable` struct allows differential dataflow programs requiring more sophisticated
-/// iterative patterns than singly recursive iteration. For example: in mutual recursion two 
+/// iterative patterns than singly recursive iteration. For example: in mutual recursion two
 /// collections evolve simultaneously.
 ///
 /// # Examples
@@ -127,7 +127,7 @@ impl<G: Scope, D: Ord+Data+Debug, R: Diff> Iterate<G, D, R> for Collection<G, D,
 ///         });
 ///     })
 /// }
-/// ```    
+/// ```
 pub struct Variable<'a, G: Scope, D: Data, R: Diff>
 where G::Timestamp: Lattice {
     collection: Collection<Child<'a, G, u64>, D, R>,
@@ -137,10 +137,14 @@ where G::Timestamp: Lattice {
 
 impl<'a, G: Scope, D: Data, R: Diff> Variable<'a, G, D, R> where G::Timestamp: Lattice {
     /// Creates a new `Variable` and a `Stream` representing its output, from a supplied `source` stream.
-    pub fn from(source: Collection<Child<'a, G, u64>, D, R>) -> Variable<'a, G, D, R> {
-        let (feedback, updates) = source.inner.scope().loop_variable(u64::max_value(), 1);
+    pub fn from_args(max_steps: u64, step: u64, source: Collection<Child<'a, G, u64>, D, R>) -> Variable<'a, G, D, R> {
+        let (feedback, updates) = source.inner.scope().loop_variable(max_steps, step);
         let collection = Collection::new(updates).concat(&source);
         Variable { collection: collection, feedback: feedback, source: source }
+    }
+    /// Creates a new `Variable` and a `Stream` representing its output, from a supplied `source` stream.
+    pub fn from(source: Collection<Child<'a, G, u64>, D, R>) -> Variable<'a, G, D, R> {
+        Self::from_args(u64::max_value(), 1, source)
     }
     /// Adds a new source of data to the `Variable`.
     pub fn set(self, result: &Collection<Child<'a, G, u64>, D, R>) -> Collection<Child<'a, G, u64>, D, R> {
