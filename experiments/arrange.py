1d781532fee75e79575c7870c05064a594d3ab7a
Andrea Lattuada <andrea.lattuada@inf.ethz.ch>
2018-04-29 15:36:57 +0200

    arrange script

--- experiments/arrange.py
+++ experiments/arrange.py
@@ -17,35 +17,45 @@ def arrange():
     experiments.eprint("### {} ###".format(experiment_name))
     experiments.eprint(experiments.experdir(experiment_name))
 
-    for w in reversed([1, 2, 4, 8, 16, 31]):
-        for nodes in [10000000]:
-            for edges in [32000000]:
-                for rate in [2500 * x for x in [2, 4, 8]]:
-                    for goal in [5]:
-                        for queries in [0, 10, 100, 1000]:
-                            for shared in ["no", "shared"]:
-                                for bidijkstra in ["no", "bidijkstra"]:
-                                    config = {
-                                        "nodes": nodes,
-                                        "edges": edges,
-                                        "rate": rate,
-                                        "goal": goal,
-                                        "queries": queries,
-                                        "shared": shared,
-                                        "bidijkstra": bidijkstra,
-                                    }
+    for w in reversed([4, 8, 16, 32]):
+        for keys in [10000000]:
+            for recs in [32000000]:
+                for rate in [1000000 * x for x in [2, 4, 8]]:
+                    for work in [1, 4, "max"]:
+                        for comp in [
+                                "exchange",
+                                "arrange",
+                                "maintain",
+                                "selfjoin",
+                                "count",
+                                "nothing",
+                                ]:
+                            mode = "closedloop"
+                            dmode = "overwrite"
+                            dparam = "10"
 
-                                    n = 1
+                            config = {
+                                "keys": keys,
+                                "recs": recs,
+                                "rate": rate,
+                                "work": work,
+                                "comp": comp,
+                                "mode": mode,
+                                "dmode": dmode,
+                                "dparam": dparam,
+                            }
 
-                                    filename = experiment_setup(experiment_name, n, w, **config)
-                                    experiments.eprint("RUNNING {}".format(filename))
-                                    commands = [
-                                            "./target/release/arrange {} -n {} -p {} -w {}".format(
-                                                " ".join(str(x) for x in [nodes, edges, rate, goal, queries, shared, bidijkstra]),
-                                                n,
-                                                p,
-                                                w) for p in range(0, n)]
-                                    experiments.eprint("commands: {}".format(commands))
-                                    processes = [experiments.run_cmd(command, filename, True) for command in commands]
-                                    experiments.waitall(processes)
+                            n = 1
+
+                            filename = experiment_setup(experiment_name, n, w, **config)
+                            experiments.eprint("RUNNING {}".format(filename))
+                            commands = [
+                                    "./target/release/arrange {} -n {} -p {} -w {}".format(
+                                        " ".join(str(x) for x in [keys, recs, rate, work, comp, mode, dmode, dparam]),
+                                        n,
+                                        p,
+                                        w) for p in range(0, n)]
+                            experiments.eprint("commands: {}".format(commands))
+                            processes = [experiments.run_cmd(command, filename, True) for command in commands]
+                            experiments.waitall(processes)
 
