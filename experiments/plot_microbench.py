91d2e610d2c06379acfd0619531f841daccbf59b
Andrea Lattuada <andrea.lattuada@inf.ethz.ch>
2018-05-01 18:22:03 +0200

    ii

--- experiments/plot_microbench.py
+++ experiments/plot_microbench.py
@@ -4,7 +4,7 @@ from os import listdir
 import sys
 
 commit = "dirty-e74441d0c062c7ec8d6da9bbf1972bd9397b2670"
-experiment = "arrange"
+experiment = "arrange-open-loop"
 print("commit {}".format(commit))
 
 def eprint(*args):
@@ -18,7 +18,7 @@ def parsename(name):
         if v.isdigit():
             v = int(v)
         return (k, v)
-    return (name, [parsekv(kv) for kv in params])
+    return (name, sorted([parsekv(kv) for kv in params], key=lambda x: x[0]))
 
 allfiles = [(parsename(f), f) for f in listdir("results/{}/{}".format(commit, experiment))]
 allparams = list(zip(*allfiles))[0]
@@ -63,7 +63,41 @@ def i_load_varies(): # commit = "dirty-8380c53277307b6e9e089a8f6f79886b36e20428"
             assert(execute('gnuplot > plots/{}/{}/i_load_varies_{}.pdf'.format(commit, experiment, groupingstr(F)), input=plotscript))
             eprint('plots/{}/{}/i_load_varies_{}.pdf'.format(commit, experiment, groupingstr(F)))
 
-    # shutil.rmtree(tempdir)
+    shutil.rmtree(tempdir)
+
+def ii_strong_scaling(): # commit = "dirty-e74441d0c062c7ec8d6da9bbf1972bd9397b2670" experiment = "arrange-open-loop"
+    tempdir = tempfile.mkdtemp("{}-{}".format(experiment, commit))
+
+    filtering = { ('rate', 2000000), }
+    for work in params['work']:
+        for comp in {'arrange', 'maintain', 'count'}:
+            F = filtering.union({ ('work', work), ('comp', comp), })
+            eprint(F)
+            # print('\n'.join(str(p) for p, f in sorted(filedict, key=lambda x: dict(x[0])['rate']) if p.issuperset(F)))
+            plotscript = "set terminal pdf size 6cm,4cm; set logscale x; set logscale y; " \
+                    "set bmargin at screen 0.2; " \
+                    "set xrange [50000:5000000000.0]; " \
+                    "set format x \"10^{%T}\"; " \
+                    "set yrange [0.005:1.01]; " \
+                    "set xlabel \"nanoseconds\"; " \
+                    "set format x \"10^{%T}\"; " \
+                    "set ylabel \"complementary cdf\"; " \
+                    "set key left bottom Left reverse font \",10\"; " \
+                    "plot "
+            dt = 2
+            for p, f in sorted(filedict, key=lambda x: dict(x[0])['w']):
+                if p.issuperset(F):
+                    datafile = "{}/i_strong_scaling_{}".format(tempdir, f)
+                    assert(execute('cat results/{}/{}/{} | grep LATENCYFRACTION | cut -f 3,4 > {}'.format(commit, experiment, f, datafile)))
+                    plotscript += "\"{}\" using 1:2 with lines lw 2 dt {} title \"{}\", ".format(datafile, dt, dict(p)['w'])
+                    dt += 1
+
+            assert(execute('mkdir -p plots/{}/{}'.format(commit, experiment)))
+            eprint(plotscript)
+            assert(execute('gnuplot > plots/{}/{}/i_strong_scaling_{}.pdf'.format(commit, experiment, groupingstr(F)), input=plotscript))
+            eprint('plots/{}/{}/i_strong_scaling_{}.pdf'.format(commit, experiment, groupingstr(F)))
+
+    shutil.rmtree(tempdir)
 
 
 def iv_throughput(): # commit = "dirty-e74441d0c062c7ec8d6da9bbf1972bd9397b2670" experiment = "arrange"
