d6edfd0015580507190f982791b3dd8aacaed454
Andrea Lattuada <andrea.lattuada@inf.ethz.ch>
2018-05-02 14:54:42 +0200

    tpch2

--- experiments/plot_tpch.sh
+++ experiments/plot_tpch.sh
@@ -62,3 +62,45 @@ gnuplot -p -e "\
    " > plots/tpch_1.pdf
 
 rm -R $temp_dir
+
+######################################
+temp_dir=$(mktemp -d)
+
+cat experiments-sf10-filtered.txt | awk '$3 == 1 && $4 == 1' | cut -f 1,5 > $temp_dir/w1
+join $temp_dir/w1 $temp_dir/w1 | awk '{ print $1,$3/$2 }' > $temp_dir/ow1
+cat experiments-sf10-filtered.txt | awk '$3 == 10 && $4 == 1' | cut -f 1,5 > $temp_dir/tw10
+join $temp_dir/w1 $temp_dir/tw10 | awk '{ print $1,$3/$2 }' > $temp_dir/w10
+cat experiments-sf10-filtered.txt | awk '$3 == 100 && $4 == 1' | cut -f 1,5 > $temp_dir/tw100
+join $temp_dir/w1 $temp_dir/tw100 | awk '{ print $1,$3/$2 }' > $temp_dir/w100
+cat experiments-sf10-filtered.txt | awk '$3 == 1000 && $4 == 1' | cut -f 1,5 > $temp_dir/tw1000
+join $temp_dir/w1 $temp_dir/tw1000 | awk '{ print $1,$3/$2 }' > $temp_dir/w1000
+cat experiments-sf10-filtered.txt | awk '$3 == 10000 && $4 == 1' | cut -f 1,5 > $temp_dir/tw10000
+join $temp_dir/w1 $temp_dir/tw10000 | awk '{ print $1,$3/$2 }' > $temp_dir/w10000
+cat experiments-sf10-filtered.txt | awk '$3 == 100000 && $4 == 1' | cut -f 1,5 > $temp_dir/tw100000
+join $temp_dir/w1 $temp_dir/tw100000 | awk '{ print $1,$3/$2 }' > $temp_dir/w100000
+cat experiments-sf10-filtered.txt | awk '$3 == 1000000 && $4 == 1' | cut -f 1,5 > $temp_dir/tw1000000
+join $temp_dir/w1 $temp_dir/tw1000000 | awk '{ print $1,$3/$2 }' > $temp_dir/w1000000
+
+gnuplot -p -e "\
+  set terminal pdf size 6cm,4cm;
+   set logscale y;
+   set rmargin at screen 0.9;
+   set bmargin at screen 0.2;
+   set xtics 2,4,22;
+   set xlabel \"query\";
+   set xrange [0:23];
+   set yrange [1:*];
+   set ylabel \"absolute throughput (tuples/sec)\";
+   set key left bottom Left reverse font \",10\";
+   plot \
+   \"$temp_dir/w1000000\" using 1:2 with lines lt 7 title \"w=10^6\", \
+   \"$temp_dir/w100000\" using 1:2 with lines lt 6 title \"w=10^5\", \
+   \"$temp_dir/w10000\" using 1:2  with lines lt 5 title \"w=10^4\", \
+   \"$temp_dir/w1000\" using 1:2  with lines lt 4 title \"w=10^3\", \
+   \"$temp_dir/w100\" using 1:2  with lines lt 3 title \"w=10^2\", \
+   \"$temp_dir/w10\" using 1:2  with lines lt 3 title \"w=10^1\"
+   " > plots/tpch_2.pdf
+
+rm -R $temp_dir
+
+######################################
