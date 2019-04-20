e0d3d66945fb84ac9bace0d3dd354b2cb94a167c
Andrea Lattuada <andrea.lattuada@inf.ethz.ch>
2019-04-20 19:52:24 +0200

    update graphs interactive plot

--- experiments/plot_graphs_interactive.py
+++ experiments/plot_graphs_interactive.py
@@ -3,14 +3,6 @@ from executor import execute
 from os import listdir
 import sys
 
-commit = sys.argv[sys.argv.index('--commit') + 1]
-experiment = sys.argv[sys.argv.index('--experiment') + 1]
-
-# commit = "dirty-e74441d0c062c7ec8d6da9bbf1972bd9397b2670"
-# experiment = "arrange-open-loop"
-print("commit {}, experiment {}".format(commit, experiment))
-
-
 def eprint(*args):
     print(*args, file=sys.stderr)
 
@@ -24,29 +16,37 @@ def parsename(name):
         return (k, v)
     return (name, sorted([parsekv(kv) for kv in params], key=lambda x: x[0]))
 
-allfiles = [(parsename(f), f) for f in listdir("results/{}/{}".format(commit, experiment))]
-allparams = list(zip(*allfiles))[0]
-pivoted = list(zip(*list(zip(*allparams))[1]))
-assert(all(all(y[0] == x[0][0] for y in x) for x in pivoted))
-params = dict(list((x[0][0], set(list(zip(*x))[1])) for x in pivoted))
-filedict = list((set(p[1]), f) for p, f in allfiles)
-eprint(params)
+def prepare(commit, experiment):
+    print("commit {}, experiment {}".format(commit, experiment))
+    allfiles = [(parsename(f), f) for f in listdir("results/{}/{}".format(commit, experiment))]
+    allparams = list(zip(*allfiles))[0]
+    pivoted = list(zip(*list(zip(*allparams))[1]))
+    assert(all(all(y[0] == x[0][0] for y in x) for x in pivoted))
+    params = dict(list((x[0][0], set(list(zip(*x))[1])) for x in pivoted))
+    filedict = list((set(p[1]), f) for p, f in allfiles)
+    eprint(params)
+    return (filedict, params)
 
 def groupingstr(s):
     return '_'.join("{}={}".format(x[0], str(x[1])) for x in sorted(s, key=lambda x: x[0]))
 
 def i_single_query():
+    commit = "73399ca494"
+    experiment = "graphs-interactive-neu-zwei"
+
+    filedict, params = prepare(commit, experiment)
+
     tempdir = tempfile.mkdtemp("{}-{}".format(experiment, commit))
 
     filtering = { ('rate', 200000), ('w', 32), ('nodes', 10000000), ('edges', 32000000), }
 
-    plotscript = "set terminal pdf size 4.8cm,3.2cm; set logscale x; set logscale y; " \
+    plotscript = "set terminal pdf size 5.2cm,3.5cm font \"Arial,10\"; set logscale x; set logscale y; " \
             "set bmargin at screen 0.2; " \
             "set xrange [10000:5000000000.0]; " \
             "set key samplen 2; " \
             "set format x \"10^{%T}\"; " \
             "set yrange [0.0001:1.01]; " \
-            "set xlabel \"nanoseconds\"; " \
+            "set xlabel \"nanoseconds\" offset 0,0.3; " \
             "set format y \"10^{%T}\"; " \
             "set bmargin at screen 0.25; " \
             "set ylabel \"complementary cdf\"; " \
@@ -77,18 +77,23 @@ def i_single_query():
     shutil.rmtree(tempdir)
 
 def ii_sharing():
+    commit = "5b0a6d19e5"
+    experiment = "graphs-interactive-neu"
+
+    filedict, params = prepare(commit, experiment)
+
     tempdir = tempfile.mkdtemp("{}-{}".format(experiment, commit))
     eprint("tempdir {}".format(tempdir))
 
     filtering = { ('rate', 200000), ('w', 32), ('nodes', 10000000), ('edges', 32000000), ('goal', 1800), }
 
-    plotscript = "set terminal pdf size 4.8cm,3.2cm; set logscale x; set logscale y; " \
+    plotscript = "set terminal pdf size 5.2cm,3.5cm font \"Arial,10\"; set logscale x; set logscale y; " \
             "set bmargin at screen 0.2; " \
             "set xrange [10000:5000000000.0]; " \
             "set key samplen 2; " \
             "set format x \"10^{%T}\"; " \
             "set yrange [0.0001:1.01]; " \
-            "set xlabel \"nanoseconds\"; " \
+            "set xlabel \"nanoseconds\" offset 0,0.3; " \
             "set format y \"10^{%T}\"; " \
             "set bmargin at screen 0.25; " \
             "set ylabel \"complementary cdf\"; " \
@@ -117,7 +122,65 @@ def ii_sharing():
 
     shutil.rmtree(tempdir)
 
+def iv_boomerang():
+    commit = "f74b611db1"
+    experiment = "graphs-interactive-neu"
+
+    filedict, params = prepare(commit, experiment)
+
+    tempdir = tempfile.mkdtemp("{}-{}".format(experiment, commit))
+    eprint("tempdir {}".format(tempdir))
+
+    filtering = { ('w', 32), ('nodes', 10000000), ('edges', 32000000), ('goal', 1800), }
+
+    plotscript = "set terminal pdf size 5.2cm,3.5cm font \"Arial,10\"; set logscale y; " \
+            "set bmargin at screen 0.2; " \
+            "set key samplen 2; " \
+            "set format y \"10^{%T}\"; " \
+            "set xlabel \"rate\" offset 0,0.3; " \
+            "set xtics 100000; " \
+            "set bmargin at screen 0.25; " \
+            "set ylabel \"nanoseconds\"; " \
+            "set key left bottom Left reverse font \",10\"; " \
+            "plot "
+
+    shared_names = {
+            'no': 'not shared',
+            'shared': 'shared',
+            }
+
+    dt = 4
+    eprint(params['shared'])
+    eprint(sorted(params['rate']))
+    for sharing in params['shared']:
+        sharing_filtering = filtering.union({ ('shared', sharing) })
+        thisname = "_".join("{}={}".format(a, b) for a, b in sharing_filtering)
+        datafile = "{}/iv_boomerang_{}".format(tempdir, thisname)
+        for rate in sorted(params['rate']):
+            rate_filtering = sharing_filtering.union({ ('rate', rate) })
+            for p, f in filedict:
+                if p.issuperset(rate_filtering):
+                    eprint(p)
+                    maxlat = execute('cat results/{}/{}/{} | grep LATENCY | awk \'{{ if ($3 < .001) {{ print $2 }} }}\' | cut -f 2 | head -n 1'.format(commit, experiment, f), capture=True)
+                    execute('echo "{}" "{}" >> {}'.format(rate, maxlat, datafile))
+        eprint(datafile)
+        plotscript += "\"{}\" using 1:2 with linespoints lw 2 dt ({}, 2) title \"{}\", ".format(datafile, dt, shared_names[sharing])
+        dt += 4
+
+    assert(execute('mkdir -p plots/{}/{}'.format(commit, experiment)))
+    eprint(plotscript)
+    execute('cat > {}/plotscript.plt'.format(tempdir), input=plotscript)
+    assert(execute('gnuplot {}/plotscript.plt > plots/{}/{}/iv_boomerang_{}.pdf'.format(tempdir, commit, experiment, groupingstr(filtering)), input=plotscript))
+    eprint('plots/{}/{}/iv_boomerang_{}.pdf'.format(commit, experiment, groupingstr(filtering)))
+
+    shutil.rmtree(tempdir)
+
 def iii_memory_rss():
+    commit = "5b0a6d19e5"
+    experiment = "graphs-interactive-neu"
+
+    filedict, params = prepare(commit, experiment)
+
     tempdir = tempfile.mkdtemp("{}-{}".format(experiment, commit))
 
     filtering = set()
@@ -125,11 +188,11 @@ def iii_memory_rss():
         F = filtering.union({ ('rate', rate), ('goal', 1800) })
         eprint(F)
 
-        plotscript = "set terminal pdf size 4.8cm,3.2cm; set logscale y; " \
+        plotscript = "set terminal pdf size 5.2cm,3.5cm font \"Arial,10\"; set logscale y; " \
                 "set bmargin at screen 0.25; " \
                 "set xrange [-200:3500]; " \
                 "set yrange [1000000000:1000000000000.0]; " \
-                "set xlabel \"elapsed seconds\"; " \
+                "set xlabel \"elapsed seconds\" offset 0,0.3; " \
                 "set xtics 0,1000,3000; " \
                 "set ylabel \"resident set size\"; " \
                 "set format y \"%.0s %cB\"; " \
@@ -157,42 +220,3 @@ def iii_memory_rss():
         assert(execute('gnuplot {}/plotscript.plt > plots/{}/{}/iii_memory_rss_{}.pdf'.format(tempdir, commit, experiment, groupingstr(F)), input=plotscript))
         eprint('plots/{}/{}/iii_memory_rss_{}.pdf'.format(commit, experiment, groupingstr(F)))
 
-# def iv_boomerang():
-#     tempdir = tempfile.mkdtemp("{}-{}".format(experiment, commit))
-# 
-#     filtering = { ('rate', 200000), ('w', 32), ('nodes', 10000000), ('edges', 32000000), ('goal', 1800), }
-# 
-#     plotscript = "set terminal pdf size 4.8cm,3.2cm; set logscale x; set logscale y; " \
-#             "set bmargin at screen 0.2; " \
-#             "set xrange [10000:5000000000.0]; " \
-#             "set key samplen 2; " \
-#             "set format x \"10^{%T}\"; " \
-#             "set yrange [0.0001:1.01]; " \
-#             "set xlabel \"nanoseconds\"; " \
-#             "set format y \"10^{%T}\"; " \
-#             "set bmargin at screen 0.25; " \
-#             "set ylabel \"complementary cdf\"; " \
-#             "set key left bottom Left reverse font \",10\"; " \
-#             "plot "
-# 
-#     shared_names = {
-#             'no': 'not shared',
-#             'shared': 'shared',
-#             }
-# 
-#     dt = 4
-#     for p, f in filedict:
-#         if p.issuperset(filtering):
-#             eprint(p)
-#             datafile = "{}/ii_sharing_{}".format(tempdir, f)
-#             assert(execute('cat results/{}/{}/{} | grep LATENCY | cut -f 2,3 > {}'.format(commit, experiment, f, datafile)))
-#             plotscript += "\"{}\" using 1:2 with lines lw 2 dt ({}, 2) title \"{}\", ".format(datafile, dt, shared_names[dict(p)['shared']])
-#             dt += 4
-# 
-#     assert(execute('mkdir -p plots/{}/{}'.format(commit, experiment)))
-#     eprint(plotscript)
-#     assert(execute('gnuplot > plots/{}/{}/iv_boomerang_{}.pdf'.format(commit, experiment, groupingstr(filtering)), input=plotscript))
-#     eprint('plots/{}/{}/ii_sharing_{}.pdf'.format(commit, experiment, groupingstr(filtering)))
-# 
-#     shutil.rmtree(tempdir)
-# 
