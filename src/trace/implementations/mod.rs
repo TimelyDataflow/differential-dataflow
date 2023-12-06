eca43dd8f9bb9037bffe35aabfdc84adc317491c
Frank McSherry <fmcsherry@me.com>
2023-12-05 19:54:31 -0500

    Huffman container example (#441)

--- src/trace/implementations/mod.rs
+++ src/trace/implementations/mod.rs
@@ -48,6 +48,7 @@ pub use self::merge_batcher::MergeBatcher as Batcher;
 // pub mod ord;
 pub mod ord_neu;
 pub mod rhh;
+pub mod huffman_container;
 
 // Opinionated takes on default spines.
 pub use self::ord_neu::OrdValSpine as ValSpine;
