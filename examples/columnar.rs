5976a2ff3903f5437e51af9c63f6db1ba469d15e
Frank McSherry <fmcsherry@me.com>
2024-12-20 16:37:28 -0500

    Work towards `Batcher` unification (#553)
    * Generalize batchers for all containers
    
    * Document and organize MergeBatcher
    
    * Make generic versions canonical
    
    * Implement account for columnation MergerChunk
    
    * Update colunmar example
    
    * Correct greivous bug in merging
    
    * Update columnar exchange function
    
    * Update src/trace/implementations/merge_batcher.rs
    
    Co-authored-by: Petros Angelatos <petrosagg@gmail.com>
    
    * Remove merge_batcher_col.rs
    
    ---------
    
    Co-authored-by: Petros Angelatos <petrosagg@gmail.com>

---
+++ examples/columnar.rs
@@ -0,0 +1,455 @@
+//! Wordcount based on `columnar`.
+
+use {
+    timely::container::CapacityContainerBuilder,
+    timely::dataflow::channels::pact::ExchangeCore,
+    timely::dataflow::InputHandleCore,
+    timely::dataflow::ProbeHandle,
+};
+
+
+use differential_dataflow::trace::implementations::ord_neu::ColKeyBuilder;
+use differential_dataflow::trace::implementations::ord_neu::ColKeySpine;
+
+use differential_dataflow::operators::arrange::arrangement::arrange_core;
+
+fn main() {
+
+    type WordCount = ((String, ()), u64, i64);
+    type Container = Column<WordCount>;
+
+    let _config = timely::Config {
+        communication: timely::CommunicationConfig::ProcessBinary(3),
+        worker: timely::WorkerConfig::default(),
+    };
+
+    let keys: usize = std::env::args().nth(1).unwrap().parse().unwrap();
+    let size: usize = std::env::args().nth(2).unwrap().parse().unwrap();
+
+    let timer1 = ::std::time::Instant::now();
+    let timer2 = timer1.clone();
+
+    // initializes and runs a timely dataflow.
+    // timely::execute(_config, move |worker| {
+    timely::execute_from_args(std::env::args(), move |worker| {
+
+        let mut data_input = <InputHandleCore<_, CapacityContainerBuilder<Container>>>::new();
+        let mut keys_input = <InputHandleCore<_, CapacityContainerBuilder<Container>>>::new();
+        let mut probe = ProbeHandle::new();
+
+        // create a new input, exchange data, and inspect its output
+        worker.dataflow::<u64, _, _>(|scope| {
+
+            let data = data_input.to_stream(scope);
+            let keys = keys_input.to_stream(scope);
+
+            let data_pact = ExchangeCore::<ColumnBuilder<((String,()),u64,i64)>,_>::new_core(|x: &((&str,()),&u64,&i64)| (x.0).0.as_bytes().iter().sum::<u8>() as u64);
+            let keys_pact = ExchangeCore::<ColumnBuilder<((String,()),u64,i64)>,_>::new_core(|x: &((&str,()),&u64,&i64)| (x.0).0.as_bytes().iter().sum::<u8>() as u64);
+
+            let data = arrange_core::<_,_,Col2KeyBatcher<_,_,_>, ColKeyBuilder<_,_,_>, ColKeySpine<_,_,_>>(&data, data_pact, "Data");
+            let keys = arrange_core::<_,_,Col2KeyBatcher<_,_,_>, ColKeyBuilder<_,_,_>, ColKeySpine<_,_,_>>(&keys, keys_pact, "Keys");
+
+            keys.join_core(&data, |_k, &(), &()| Option::<()>::None)
+                .probe_with(&mut probe);
+
+        });
+
+        // Load up data in batches.
+        let mut counter = 0;
+        while counter < 10 * keys {
+            let mut i = worker.index();
+            let time = *data_input.time();
+            while i < size {
+                let val = (counter + i) % keys;
+                data_input.send(((&format!("{:?}", val), ()), time, 1));
+                i += worker.peers();
+            }
+            counter += size;
+            data_input.advance_to(data_input.time() + 1);
+            keys_input.advance_to(keys_input.time() + 1);
+            while probe.less_than(data_input.time()) {
+                worker.step();
+            }
+        }
+        println!("{:?}\tloading complete", timer1.elapsed());
+
+        let mut queries = 0;
+
+        while queries < 10 * keys {
+            let mut i = worker.index();
+            let time = *data_input.time();
+            while i < size {
+                let val = (queries + i) % keys;
+                data_input.send(((&format!("{:?}", val), ()), time, 1));
+                i += worker.peers();
+            }
+            queries += size;
+            data_input.advance_to(data_input.time() + 1);
+            keys_input.advance_to(keys_input.time() + 1);
+            while probe.less_than(data_input.time()) {
+                worker.step();
+            }
+        }
+
+        println!("{:?}\tqueries complete", timer1.elapsed());
+
+
+    })
+    .unwrap();
+
+    println!("{:?}\tshut down", timer2.elapsed());
+}
+
+
+pub use container::Column;
+mod container {
+
+    use columnar::Columnar;
+    use columnar::Container as FooBozzle;
+
+    use timely::bytes::arc::Bytes;
+
+    /// A container based on a columnar store, encoded in aligned bytes.
+    pub enum Column<C: Columnar> {
+        /// The typed variant of the container.
+        Typed(C::Container),
+        /// The binary variant of the container.
+        Bytes(Bytes),
+        /// Relocated, aligned binary data, if `Bytes` doesn't work for some reason.
+        ///
+        /// Reasons could include misalignment, cloning of data, or wanting
+        /// to release the `Bytes` as a scarce resource.
+        Align(Box<[u64]>),
+    }
+
+    impl<C: Columnar> Default for Column<C> {
+        fn default() -> Self { Self::Typed(Default::default()) }
+    }
+
+    impl<C: Columnar> Clone for Column<C> where C::Container: Clone {
+        fn clone(&self) -> Self {
+            match self {
+                Column::Typed(t) => Column::Typed(t.clone()),
+                Column::Bytes(b) => {
+                    assert!(b.len() % 8 == 0);
+                    let mut alloc: Vec<u64> = vec![0; b.len() / 8];
+                    bytemuck::cast_slice_mut(&mut alloc[..]).copy_from_slice(&b[..]);
+                    Self::Align(alloc.into())
+                },
+                Column::Align(a) => Column::Align(a.clone()),
+            }
+        }
+    }
+
+    use columnar::{Clear, Len, Index, AsBytes, FromBytes};
+    use columnar::bytes::serialization::decode;
+    use columnar::common::IterOwn;
+
+    use timely::Container;
+    impl<C: Columnar> Container for Column<C> {
+        fn len(&self) -> usize {
+            match self {
+                Column::Typed(t) => t.len(),
+                Column::Bytes(b) => <<C::Container as columnar::Container<C>>::Borrowed<'_> as FromBytes>::from_bytes(&mut decode(bytemuck::cast_slice(b))).len(),
+                Column::Align(a) => <<C::Container as columnar::Container<C>>::Borrowed<'_> as FromBytes>::from_bytes(&mut decode(a)).len(),
+            }
+        }
+        // This sets the `Bytes` variant to be an empty `Typed` variant, appropriate for pushing into.
+        fn clear(&mut self) {
+            match self {
+                Column::Typed(t) => t.clear(),
+                Column::Bytes(_) => *self = Column::Typed(Default::default()),
+                Column::Align(_) => *self = Column::Typed(Default::default()),
+            }
+        }
+
+        type ItemRef<'a> = C::Ref<'a>;
+        type Iter<'a> = IterOwn<<C::Container as columnar::Container<C>>::Borrowed<'a>>;
+        fn iter<'a>(&'a self) -> Self::Iter<'a> {
+            match self {
+                Column::Typed(t) => t.borrow().into_iter(),
+                Column::Bytes(b) => <<C::Container as columnar::Container<C>>::Borrowed<'a> as FromBytes>::from_bytes(&mut decode(bytemuck::cast_slice(b))).into_iter(),
+                Column::Align(a) => <<C::Container as columnar::Container<C>>::Borrowed<'a> as FromBytes>::from_bytes(&mut decode(a)).into_iter(),
+            }
+        }
+
+        type Item<'a> = C::Ref<'a>;
+        type DrainIter<'a> = IterOwn<<C::Container as columnar::Container<C>>::Borrowed<'a>>;
+        fn drain<'a>(&'a mut self) -> Self::DrainIter<'a> {
+            match self {
+                Column::Typed(t) => t.borrow().into_iter(),
+                Column::Bytes(b) => <<C::Container as columnar::Container<C>>::Borrowed<'a> as FromBytes>::from_bytes(&mut decode(bytemuck::cast_slice(b))).into_iter(),
+                Column::Align(a) => <<C::Container as columnar::Container<C>>::Borrowed<'a> as FromBytes>::from_bytes(&mut decode(a)).into_iter(),
+            }
+        }
+    }
+
+    use timely::container::SizableContainer;
+    impl<C: Columnar> SizableContainer for Column<C> {
+        fn at_capacity(&self) -> bool {
+            match self {
+                Self::Typed(t) => {
+                    let length_in_bytes = t.borrow().length_in_words() * 8;
+                    length_in_bytes >= (1 << 20)
+                },
+                Self::Bytes(_) => true,
+                Self::Align(_) => true,
+            }
+        }
+        fn ensure_capacity(&mut self, _stash: &mut Option<Self>) { }
+    }
+
+    use timely::container::PushInto;
+    impl<C: Columnar, T> PushInto<T> for Column<C> where C::Container: columnar::Push<T> {
+        #[inline]
+        fn push_into(&mut self, item: T) {
+            use columnar::Push;
+            match self {
+                Column::Typed(t) => t.push(item),
+                Column::Align(_) | Column::Bytes(_) => {
+                    // We really oughtn't be calling this in this case.
+                    // We could convert to owned, but need more constraints on `C`.
+                    unimplemented!("Pushing into Column::Bytes without first clearing");
+                }
+            }
+        }
+    }
+
+    use timely::dataflow::channels::ContainerBytes;
+    impl<C: Columnar> ContainerBytes for Column<C> {
+        fn from_bytes(bytes: timely::bytes::arc::Bytes) -> Self {
+            // Our expectation / hope is that `bytes` is `u64` aligned and sized.
+            // If the alignment is borked, we can relocate. IF the size is borked,
+            // not sure what we do in that case.
+            assert!(bytes.len() % 8 == 0);
+            if let Ok(_) = bytemuck::try_cast_slice::<_, u64>(&bytes) {
+                Self::Bytes(bytes)
+            }
+            else {
+                // println!("Re-locating bytes for alignment reasons");
+                let mut alloc: Vec<u64> = vec![0; bytes.len() / 8];
+                bytemuck::cast_slice_mut(&mut alloc[..]).copy_from_slice(&bytes[..]);
+                Self::Align(alloc.into())
+            }
+        }
+
+        fn length_in_bytes(&self) -> usize {
+            match self {
+                // We'll need one u64 for the length, then the length rounded up to a multiple of 8.
+                Column::Typed(t) => 8 * t.borrow().length_in_words(),
+                Column::Bytes(b) => b.len(),
+                Column::Align(a) => 8 * a.len(),
+            }
+        }
+
+        fn into_bytes<W: ::std::io::Write>(&self, writer: &mut W) {
+            match self {
+                Column::Typed(t) => {
+                    use columnar::Container;
+                    // Columnar data is serialized as a sequence of `u64` values, with each `[u8]` slice
+                    // serialize as first its length in bytes, and then as many `u64` values as needed.
+                    // Padding should be added, but only for alignment; no specific values are required.
+                    for (align, bytes) in t.borrow().as_bytes() {
+                        assert!(align <= 8);
+                        let length: u64 = bytes.len().try_into().unwrap();
+                        writer.write_all(bytemuck::cast_slice(std::slice::from_ref(&length))).unwrap();
+                        writer.write_all(bytes).unwrap();
+                        let padding: usize = ((8 - (length % 8)) % 8).try_into().unwrap();
+                        writer.write_all(&[0; 8][..padding]).unwrap();
+                    }
+                },
+                Column::Bytes(b) => writer.write_all(b).unwrap(),
+                Column::Align(a) => writer.write_all(bytemuck::cast_slice(a)).unwrap(),
+            }
+        }
+    }
+}
+
+
+use builder::ColumnBuilder;
+mod builder {
+
+    use std::collections::VecDeque;
+    use columnar::{Columnar, Clear, Len, AsBytes, Push};
+    use super::Column;
+
+    /// A container builder for `Column<C>`.
+    pub struct ColumnBuilder<C: Columnar> {
+        /// Container that we're writing to.
+        current: C::Container,
+        /// Empty allocation.
+        empty: Option<Column<C>>,
+        /// Completed containers pending to be sent.
+        pending: VecDeque<Column<C>>,
+    }
+
+    use timely::container::PushInto;
+    impl<C: Columnar, T> PushInto<T> for ColumnBuilder<C> where C::Container: columnar::Push<T> {
+        #[inline]
+        fn push_into(&mut self, item: T) {
+            self.current.push(item);
+            // If there is less than 10% slop with 2MB backing allocations, mint a container.
+            use columnar::Container;
+            let words = self.current.borrow().length_in_words();
+            let round = (words + ((1 << 18) - 1)) & !((1 << 18) - 1);
+            if round - words < round / 10 {
+                let mut alloc = Vec::with_capacity(round);
+                columnar::bytes::serialization::encode(&mut alloc, self.current.borrow().as_bytes());
+                self.pending.push_back(Column::Align(alloc.into_boxed_slice()));
+                self.current.clear();
+            }
+        }
+    }
+
+    impl<C: Columnar> Default for ColumnBuilder<C> {
+        fn default() -> Self {
+            ColumnBuilder {
+                current: Default::default(),
+                empty: None,
+                pending: Default::default(),
+            }
+        }
+    }
+
+    use timely::container::{ContainerBuilder, LengthPreservingContainerBuilder};
+    impl<C: Columnar> ContainerBuilder for ColumnBuilder<C> where C::Container: Clone {
+        type Container = Column<C>;
+
+        #[inline]
+        fn extract(&mut self) -> Option<&mut Self::Container> {
+            if let Some(container) = self.pending.pop_front() {
+                self.empty = Some(container);
+                self.empty.as_mut()
+            } else {
+                None
+            }
+        }
+
+        #[inline]
+        fn finish(&mut self) -> Option<&mut Self::Container> {
+            if !self.current.is_empty() {
+                self.pending.push_back(Column::Typed(std::mem::take(&mut self.current)));
+            }
+            self.empty = self.pending.pop_front();
+            self.empty.as_mut()
+        }
+    }
+
+    impl<C: Columnar> LengthPreservingContainerBuilder for ColumnBuilder<C> where C::Container: Clone { }
+}
+
+
+use differential_dataflow::trace::implementations::merge_batcher::MergeBatcher;
+use differential_dataflow::trace::implementations::merge_batcher::ColMerger;
+use timely::container::columnation::TimelyStack;
+
+/// A batcher for columnar storage.
+pub type Col2ValBatcher<K, V, T, R> = MergeBatcher<Column<((K,V),T,R)>, batcher::Chunker<TimelyStack<((K,V),T,R)>>, ColMerger<(K,V),T,R>>;
+pub type Col2KeyBatcher<K, T, R> = Col2ValBatcher<K, (), T, R>;
+
+/// Types for consolidating, merging, and extracting columnar update collections.
+pub mod batcher {
+
+    use std::collections::VecDeque;
+    use columnar::Columnar;
+    use timely::Container;
+    use timely::container::{ContainerBuilder, PushInto};
+    use differential_dataflow::difference::Semigroup;
+    use crate::Column;
+
+    // First draft: build a "chunker" and a "merger".
+
+    #[derive(Default)]
+    pub struct Chunker<C> {
+        /// Buffer into which we'll consolidate.
+        ///
+        /// Also the buffer where we'll stage responses to `extract` and `finish`.
+        /// When these calls return, the buffer is available for reuse.
+        empty: C,
+        /// Consolidated buffers ready to go.
+        ready: VecDeque<C>,
+    }
+
+    impl<C: Container + Clone + 'static> ContainerBuilder for Chunker<C> {
+        type Container = C;
+
+        fn extract(&mut self) -> Option<&mut Self::Container> {
+            if let Some(ready) = self.ready.pop_front() {
+                self.empty = ready;
+                Some(&mut self.empty)
+            } else {
+                None
+            }
+        }
+
+        fn finish(&mut self) -> Option<&mut Self::Container> {
+            self.extract()
+        }
+    }
+
+    impl<'a, D, T, R, C2> PushInto<&'a mut Column<(D, T, R)>> for Chunker<C2> 
+    where
+        D: Columnar,
+        for<'b> D::Ref<'b>: Ord + Copy,
+        T: Columnar,
+        for<'b> T::Ref<'b>: Ord + Copy,
+        R: Columnar + Semigroup + for<'b> Semigroup<R::Ref<'b>>,
+        for<'b> R::Ref<'b>: Ord,
+        C2: Container + for<'b> PushInto<&'b (D, T, R)>,
+    {
+        fn push_into(&mut self, container: &'a mut Column<(D, T, R)>) {
+
+            // Scoped to let borrow through `permutation` drop.
+            {
+                // Sort input data
+                // TODO: consider `Vec<usize>` that we retain, containing indexes.
+                let mut permutation = Vec::with_capacity(container.len());
+                permutation.extend(container.drain());
+                permutation.sort();
+
+                self.empty.clear();
+                // Iterate over the data, accumulating diffs for like keys.
+                let mut iter = permutation.drain(..);
+                if let Some((data, time, diff)) = iter.next() {
+
+                    let mut owned_data = D::into_owned(data);
+                    let mut owned_time = T::into_owned(time);
+
+                    let mut prev_data = data;
+                    let mut prev_time = time;
+                    let mut prev_diff = <R as Columnar>::into_owned(diff);
+
+                    for (data, time, diff) in iter {
+                        if (&prev_data, &prev_time) == (&data, &time) {
+                            prev_diff.plus_equals(&diff);
+                        }
+                        else {
+                            if !prev_diff.is_zero() {
+                                D::copy_from(&mut owned_data, prev_data);
+                                T::copy_from(&mut owned_time, prev_time);
+                                let tuple = (owned_data, owned_time, prev_diff);
+                                self.empty.push_into(&tuple);
+                                owned_data = tuple.0;
+                                owned_time = tuple.1;
+                            }
+                            prev_data = data;
+                            prev_time = time;
+                            prev_diff = <R as Columnar>::into_owned(diff);
+                        }
+                    }
+
+                    if !prev_diff.is_zero() {
+                        D::copy_from(&mut owned_data, prev_data);
+                        T::copy_from(&mut owned_time, prev_time);
+                        let tuple = (owned_data, owned_time, prev_diff);
+                        self.empty.push_into(&tuple);
+                    }
+                }
+            }
+
+            if !self.empty.is_empty() {
+                self.ready.push_back(std::mem::take(&mut self.empty));
+            }
+        }
+    }
+}
